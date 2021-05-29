fcac8edd4b71c293203268f9507ecd6b5b20239a
Mark J Price <markjprice@gmail.com>
2021-05-29 13:48:39 +0100

    Add CodeAnalyzing project

---
+++ vs4win/Chapter06/CodeAnalyzing/Program.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="Program.cs" company="Packt">
+// Copyright (c) Packt. All rights reserved.
+// </copyright>
+
+namespace CodeAnalyzing
+{
+  using System;
+
+  /// <summary>
+  /// The main class for this console app.
+  /// </summary>
+  public class Program
+  {
+    /// <summary>
+    /// The main entry point for this console app.
+    /// </summary>
+    /// <param name="args">A string array of arguments passed to the console app.</param>
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
