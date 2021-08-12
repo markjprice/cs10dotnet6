9f802655618e9ab94c38ac51c461f10d8fe98f8b
Mark J Price <markjprice@gmail.com>
2021-08-12 21:42:47 +0100

    Updated for Preview 7

--- vs4win/Chapter02/Vocabulary/Program.cs
+++ vs4win/Chapter02/Vocabulary/Program.cs
@@ -1,43 +1,34 @@
-﻿using System;
-using System.Linq;
+﻿// See https://aka.ms/new-console-template for more information
+// #error version
 using System.Reflection;
 
-// #error version
+// declare some unused variables using types
+// in additional assemblies
+System.Data.DataSet ds;
+HttpClient client;
 
-namespace Vocabulary
+Assembly? assembly = Assembly.GetEntryAssembly();
+if (assembly == null) return;
+
+// loop through the assemblies that this app references
+foreach (AssemblyName name in assembly.GetReferencedAssemblies())
 {
-  class Program
+  // load the assembly so we can read its details
+  Assembly a = Assembly.Load(name);
+
+  // declare a variable to count the number of methods
+  int methodCount = 0;
+
+  // loop through all the types in the assembly
+  foreach (TypeInfo t in a.DefinedTypes)
   {
-    static void Main(string[] args)
-    {
-      // declare some unused variables using types
-      // in additional assemblies
-      System.Data.DataSet ds;
-      System.Net.Http.HttpClient client;
-
-      // loop through the assemblies that this app references 
-      foreach (AssemblyName name in Assembly
-        .GetEntryAssembly().GetReferencedAssemblies())
-      {
-        // load the assembly so we can read its details
-        Assembly a = Assembly.Load(name);
-
-        // declare a variable to count the number of methods
-        int methodCount = 0;
-
-        // loop through all the types in the assembly 
-        foreach (TypeInfo t in a.DefinedTypes)
-        {
-          // add up the counts of methods 
-          methodCount += t.GetMethods().Length;
-        }
-        
-        // output the count of types and their methods
-        Console.WriteLine(
-          "{0:N0} types with {1:N0} methods in {2} assembly.",
-          arg0: a.DefinedTypes.Count(),
-          arg1: methodCount, arg2: name.Name);
-      }
-    }
+    // add up the counts of methods
+    methodCount += t.GetMethods().Count();
   }
+
+  // output the count of types and their methods
+  Console.WriteLine(
+    "{0:N0} types with {1:N0} methods in {2} assembly.",
+    arg0: a.DefinedTypes.Count(),
+    arg1: methodCount, arg2: name.Name);
 }
--- vscode/Chapter02/Formatting/Program.cs
+++ vscode/Chapter02/Formatting/Program.cs
@@ -1,66 +1,56 @@
-﻿using System;
-using static System.Console;
+﻿using static System.Console;
 
-namespace Formatting
-{
-  class Program
-  {
-    static void Main(string[] args)
-    {
-      int numberOfApples = 12;
-      decimal pricePerApple = 0.35M;
+int numberOfApples = 12;
+decimal pricePerApple = 0.35M;
 
-      WriteLine(
-          format: "{0} apples costs {1:C}",
-          arg0: numberOfApples,
-          arg1: pricePerApple * numberOfApples);
+WriteLine(
+    format: "{0} apples costs {1:C}",
+    arg0: numberOfApples,
+    arg1: pricePerApple * numberOfApples);
 
-      string formatted = string.Format(
-          format: "{0} apples costs {1:C}",
-          arg0: numberOfApples,
-          arg1: pricePerApple * numberOfApples);
+string formatted = string.Format(
+    format: "{0} apples costs {1:C}",
+    arg0: numberOfApples,
+    arg1: pricePerApple * numberOfApples);
 
-      //WriteToFile(formatted); // writes the string into a file
+//WriteToFile(formatted); // writes the string into a file
 
-      WriteLine($"{numberOfApples} apples costs {pricePerApple * numberOfApples:C}");
+WriteLine($"{numberOfApples} apples costs {pricePerApple * numberOfApples:C}");
 
-      string applesText = "Apples";
-      int applesCount = 1234;
-      
-      string bananasText = "Bananas";
-      int bananasCount = 56789;
+string applesText = "Apples";
+int applesCount = 1234;
 
-      WriteLine(
-          format: "{0,-8} {1,6:N0}",
-          arg0: "Name",
-          arg1: "Count");
+string bananasText = "Bananas";
+int bananasCount = 56789;
 
-      WriteLine(
-          format: "{0,-8} {1,6:N0}",
-          arg0: applesText,
-          arg1: applesCount);
+WriteLine(
+    format: "{0,-8} {1,6:N0}",
+    arg0: "Name",
+    arg1: "Count");
 
-      WriteLine(
-          format: "{0,-8} {1,6:N0}",
-          arg0: bananasText,
-          arg1: bananasCount);
+WriteLine(
+    format: "{0,-8} {1,6:N0}",
+    arg0: applesText,
+    arg1: applesCount);
 
-      Write("Type your first name and press ENTER: ");
-      string firstName = ReadLine();
+WriteLine(
+    format: "{0,-8} {1,6:N0}",
+    arg0: bananasText,
+    arg1: bananasCount);
 
-      Write("Type your age and press ENTER: ");
-      string age = ReadLine();
+Write("Type your first name and press ENTER: ");
+string? firstName = ReadLine();
 
-      WriteLine(
-        $"Hello {firstName}, you look good for {age}.");
+Write("Type your age and press ENTER: ");
+string? age = ReadLine();
 
-      Write("Press any key combination: ");
-      ConsoleKeyInfo key = ReadKey();
-      WriteLine();
-      WriteLine("Key: {0}, Char: {1}, Modifiers: {2}",
-        arg0: key.Key,
-        arg1: key.KeyChar,
-        arg2: key.Modifiers);
-    }
-  }
-}
+WriteLine(
+  $"Hello {firstName}, you look good for {age}.");
+
+Write("Press any key combination: ");
+ConsoleKeyInfo key = ReadKey();
+WriteLine();
+WriteLine("Key: {0}, Char: {1}, Modifiers: {2}",
+  arg0: key.Key,
+  arg1: key.KeyChar,
+  arg2: key.Modifiers);
