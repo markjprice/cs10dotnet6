dad176d816b08de2975d7fe2c5b8c5c4fee3e196
Mark J Price <markjprice@gmail.com>
2021-10-10 17:24:17 +0100

    Improved for locales
    Added curlies to output correctly in locales

--- vs4win/Chapter02/Numbers/Program.cs
+++ vs4win/Chapter02/Numbers/Program.cs
@@ -33,11 +33,11 @@ double b = 0.2;
 
 if (a + b == 0.3)
 {
-  Console.WriteLine($"{a} + {b} equals 0.3");
+  Console.WriteLine($"{a} + {b} equals {0.3}");
 }
 else
 {
-  Console.WriteLine($"{a} + {b} does NOT equal 0.3");
+  Console.WriteLine($"{a} + {b} does NOT equal {0.3}");
 }
 
 Console.WriteLine("Using decimals:");
