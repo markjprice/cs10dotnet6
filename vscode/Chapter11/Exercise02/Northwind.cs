5da4d4036f1ee241d74f622f454571af3574c365
Mark J Price <markjprice@gmail.com>
2021-10-27 18:42:09 +0100

    Rename some chapters

--- vscode/Chapter11/Exercise02/Northwind.cs
+++ vscode/Chapter11/Exercise02/Northwind.cs
@@ -1,36 +1,21 @@
-using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore; // DbContext
+
+using static System.Environment;
+using static System.IO.Path;
 
 namespace Packt.Shared;
 
-// this manages the connection to the database
+// this manages the connection to the database 
 public class Northwind : DbContext
 {
   // these properties map to tables in the database 
-  public DbSet<Category>? Categories { get; set; }
-  public DbSet<Product>? Products { get; set; }
+  public DbSet<Customer>? Customers { get; set; }
 
   protected override void OnConfiguring(
     DbContextOptionsBuilder optionsBuilder)
   {
-    string path = Path.Combine(
-      Environment.CurrentDirectory, "Northwind.db");
+    string path = Combine(CurrentDirectory, "Northwind.db");
 
     optionsBuilder.UseSqlite($"Filename={path}");
-  }
-
-  protected override void OnModelCreating(
-    ModelBuilder modelBuilder)
-  {
-    // example of using Fluent API instead of attributes
-    // to limit the length of a category name to under 15
-    modelBuilder.Entity<Category>()
-      .Property(category => category.CategoryName)
-      .IsRequired() // NOT NULL
-      .HasMaxLength(15);
-
-    // added to "fix" the decimal support in SQLite
-    modelBuilder.Entity<Product>()
-      .Property(product => product.Cost)
-      .HasConversion<double>();
   }
 }
