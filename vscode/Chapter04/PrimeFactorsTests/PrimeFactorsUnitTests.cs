b353ae9016dd729bb6a2adee75ee1f89bbd8915f
Mark J Price <markjprice@gmail.com>
2022-05-10 08:01:06 +0100

    Added prime numbers up to 1000

--- vscode/Chapter04/PrimeFactorsTests/PrimeFactorsUnitTests.cs
+++ vscode/Chapter04/PrimeFactorsTests/PrimeFactorsUnitTests.cs
@@ -31,5 +31,47 @@ namespace PrimeFactors
       // assert
       Assert.Equal(expected, actual);
     }
+
+    [Fact]
+    public void PrimeFactorsOf519()
+    {
+      // arrange
+      int number = 519;
+      string expected = "173 x 3";
+
+      // act
+      string actual = Primes.PrimeFactors(number);
+
+      // assert
+      Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void PrimeFactorsOf997()
+    {
+      // arrange
+      int number = 997;
+      string expected = "997";
+
+      // act
+      string actual = Primes.PrimeFactors(number);
+
+      // assert
+      Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void PrimeFactorsOf1000()
+    {
+      // arrange
+      int number = 1000;
+      string expected = "5 x 5 x 5 x 2 x 2 x 2";
+
+      // act
+      string actual = Primes.PrimeFactors(number);
+
+      // assert
+      Assert.Equal(expected, actual);
+    }
   }
 }
