c4fc66648a1a1d54fb1fee14f0dd3d6d343f0013
David Nguyen <aisdavid9@gmail.com>
2022-06-20 16:20:15 +0700

    Progress Day #1

---
+++ vs4win/Chapter02/Formatting/Formatting Exercise/Program.cs
@@ -0,0 +1,31 @@
+﻿int numberOfApples = 12;
+decimal pricePerApple = 0.35M;
+
+Console.WriteLine(
+  format: "{0} apples costs {1:C}",
+  arg0: numberOfApples,
+  arg1: pricePerApple * numberOfApples);
+
+//The WriteToFile method is a nonexistent method used to illustrate the idea.
+
+//Next exercise
+string applesText = "Apples";
+int applesCount = 1234;
+string bananasText = "Bananas";
+int bananasCount = 56789;
+
+//Inputs the table headers
+Console.WriteLine(
+  format: "{0,-10} {1,6:N0}",
+  arg0: "Name",
+  arg1: "Count");
+
+//Specifies the rest of the tabular format
+Console.WriteLine(
+  format: "{0,-10} {1,6:N0}",
+  arg0: applesText,
+  arg1: applesCount);
+Console.WriteLine(
+  format: "{0,-10} {1,6:N0}",
+  arg0: bananasText,
+  arg1: bananasCount);
